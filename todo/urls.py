ecbf49df4d54d4635cbd69ee1c227578d8370d76
sanket ghode <sanketrghode@gmail.com>
2025-11-09 13:19:44 +0530

    Initial commit - ToDo Web App

---
+++ todo/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('addTask/', views.addTask, name='addTask'),
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
+    path('edit_task/<int:pk>/', views.edit_Task, name='edit_task'),
+    path('delete_task/<int:pk>/', views.delete_Task, name='delete_task'),
+]
